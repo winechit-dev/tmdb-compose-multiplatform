c98c4fd5bd1c1c270598f3fbd712912d5ce59448
winechit-dev <winechitpaing.dev@gmail.com>
2025-04-22 12:16:57 +0700

    update JVM target and compatibility to version 21

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -15,7 +15,7 @@ kotlin {
     androidTarget {
         @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
+            jvmTarget.set(JvmTarget.JVM_21)
         }
     }
     
@@ -78,8 +78,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 }
 
