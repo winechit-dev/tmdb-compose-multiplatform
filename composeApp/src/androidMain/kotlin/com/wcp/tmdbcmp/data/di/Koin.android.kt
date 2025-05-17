304ee2c7acc5da2aa90669da61d77db56359f3d3
winechit-dev <winechitpaing.dev@gmail.com>
2025-05-17 16:16:41 +0700

    add Koin platform modules for dependency injection support

---
+++ composeApp/src/androidMain/kotlin/com/wcp/tmdbcmp/data/di/Koin.android.kt
@@ -0,0 +1,13 @@
+package com.wcp.tmdbcmp.data.di
+
+import com.wcp.tmdbcmp.data.datastore.PreferencesDataSource
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+actual fun platformModule(): Module {
+    return module {
+        single<PreferencesDataSource> {
+            Factory(get()).createPreferencesDataSource()
+        }
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/wcp/tmdbcmp/data/di/Koin.kt
@@ -0,0 +1,5 @@
+package com.wcp.tmdbcmp.data.di
+
+import org.koin.core.module.Module
+
+expect fun platformModule() : Module
