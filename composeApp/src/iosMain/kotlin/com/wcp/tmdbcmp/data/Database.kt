73110c22cea5d5b58f3fc3c610f0360cb15ca129
winechit-dev <winechitpaing.dev@gmail.com>
2025-05-18 14:50:49 +0700

    add Room database imports for AppDatabase integration

--- composeApp/src/iosMain/kotlin/com/wcp/tmdbcmp/data/Database.kt
+++ composeApp/src/iosMain/kotlin/com/wcp/tmdbcmp/data/Database.kt
@@ -1,5 +1,8 @@
 package com.wcp.tmdbcmp.data
 
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.wcp.tmdbcmp.data.database.AppDatabase
 import com.wcp.tmdbcmp.data.database.DB_FILE_NAME
 
 fun getDatabaseBuilder(): RoomDatabase.Builder<AppDatabase> {
